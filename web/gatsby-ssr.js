4d2f6866bb547ac292b1d837a937a28e5041be10
Ryan Jansen <rjgiga444@gmail.com>
2021-05-26 00:29:34 +0800

    Added panelbear analytics

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -5,3 +5,31 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react";
+
+const PANELBEAR_SITE_ID = "5zCMCawNI7i";
+
+const PANELBEAR_CONFIG = {
+  site: PANELBEAR_SITE_ID,
+  spaMode: "history",
+  debug: false,
+};
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      key="panelbear-analytics-src"
+      async
+      src={`https://cdn.panelbear.com/analytics.js?site=${PANELBEAR_SITE_ID}`}
+    />,
+    <script
+      key="panelbear-analytics-code"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.panelbear = window.panelbear || function() { (window.panelbear.q = window.panelbear.q || []).push(arguments); };
+          panelbear('config', ${JSON.stringify(PANELBEAR_CONFIG)});
+        `,
+      }}
+    />,
+  ]);
+};
